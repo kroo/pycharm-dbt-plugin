9c1890095f523ef3838152da39211caa9072cb2e
GitHub Action <action@github.com>
2020-07-30 01:16:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/kroo/pycharmdbtplugin/MyBundle.kt
+++ src/main/kotlin/com/github/kroo/pycharmdbtplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kroo.pycharmdbtplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kroo/pycharmdbtplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kroo/pycharmdbtplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kroo.pycharmdbtplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kroo.pycharmdbtplugin.MyBundle
 
 class MyApplicationService {
 
